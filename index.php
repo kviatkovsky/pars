c345b04ebbb9ac36686705d7408a949216ef86ce
kviatkovsky <kviatkovsky.v@gmail.com>
2016-04-26 21:03:15 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,43 +4,45 @@ function docx2text($filename) {
     return getTextFromZippedXML($filename, "word/document.xml");
 }
 function getTextFromZippedXML($archiveFile, $contentFile) {
-    
+   
     $zip = new ZipArchive;
-    
+   
     if ($zip->open($archiveFile)) {
-       
+     
         if (($index = $zip->locateName($contentFile)) !== false) {
            
             $content = $zip->getFromIndex($index);
-            
+         
             $zip->close();
  
            
             $xml = DOMDocument::loadXML($content, LIBXML_NOENT | LIBXML_XINCLUDE | LIBXML_NOERROR | LIBXML_NOWARNING);
  
-            $string = iconv("UTF-8", "Windows-1251", $xml->saveXML());
+            $string = $xml->saveXML();
  
-            $perenos_xml = array("</w:t>", "</w:p>", "<w:br/>");
+            $perenos_xml = array("</w:p>", "<w:br/>", "<w:i/>");
             $perenos_norm = "<br />";
  
-           
+            
             $new_perenos = str_replace($perenos_xml, $perenos_norm, $string);
  
- 
-          
-            return strip_tags($xml->saveXML(), "<br />");
+         
+            return $new_perenos;
         }
         $zip->close();
     }
-   
-    return '';
-
+    
+    return "";
 }
 
- $a = docx2text("text.docx"); 
- var_dump ($a);
- $result_array = preg_match_all("/\d{1,2}.\d{1,2} – \d{1,2}.\d{1,2}/",$a,$matches);
+ $text = docx2text("texttest.docx.docx"); 
+ echo $text;
+ $result_array = preg_match_all("/\d{1,2}.\d{1,2} – \d{1,2}.\d{1,2}/",$text,$matches);
    
    echo "<pre>";
 		print_r($matches);
+		echo "</pre>";
+		$result_array = preg_match_all("/<w:t>.*<\/w:t>/U",$text,$matches2);
+   echo "<pre>";
+		print_r($matches2);
 		echo "</pre>";
