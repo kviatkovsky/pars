f0d33dc50312c1f36d40bcbb2c10f3ff2ede5c18
kviatkovsky <kviatkovsky.v@gmail.com>
2016-04-27 04:51:32 +0300

    Update index.php

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ function getTextFromZippedXML($archiveFile, $contentFile) {
  
             $string = $xml->saveXML();
  
-            $tag_xml = array("</w:p>", "<w:br/>", "<w:i/>");
+            $tag_xml = array("</w:p>", "<w:br/>");
             $tag = "<br />";
  
             
@@ -34,15 +34,72 @@ function getTextFromZippedXML($archiveFile, $contentFile) {
     
     return "";
 }
-
  $text = docx2text("texttest.docx.docx"); 
  echo $text;
  $result_array = preg_match_all("/\d{1,2}.\d{1,2} – \d{1,2}.\d{1,2}/",$text,$matches);
    
    echo "<pre>";
-		print_r($matches);
+   echo "//---------------------------1111111111111--------------------------+++++++++++++<br>";
+   $countMathes =  count($matches2);
+		$date = array();
+		for($i=0;$i<=$countMathes;$i++){
+			$date = $matches[$i];
+		}
+		//print_r($matches);
 		echo "</pre>";
-		$result_array = preg_match_all("/<w:t>.*<\/w:t>/U",$text,$matches2);
+		echo $date[1];
+		$result_array = preg_match_all("/\d{1,2}.\d{1,2} – \d{1,2}.\d{1,2}(.*br.*br)/U",$text,$matches2);
    echo "<pre>";
-		print_r($matches2);
+   echo "//---------------------------222222222222----------------------------++++++++++++<br>";
+     $countMathes =  count($matches2);
+	 
+       // print_r($matches2);
+	   $location = array();
+	   for($i=0;$i<=$countMathes;$i++){
+		$a = $matches2[0][$i];
+		//echo $a;
+		$result_array = preg_match("/<w:t>.*<\/w:t>/",$a,$matchesa);
+			//print_r($matchesa);
+				
+					$location[$i] = $matchesa[0];
+				
+	   }
+	   
+		//print_r($matchesa);
+		echo $location[2];
+	      
+		 
+       // print_r($matches2);
+		
+		//print_r($matchesa);
+		echo "</pre>";
+		
+		$result_array = preg_match_all("/\d{1,2}.\d{1,2} – \d{1,2}.\d{1,2}(.*br.*br.*br)/U",$text,$matches3);
+		
+   echo "<pre>";
+   echo "//---------------------------3333333333333---------------------------------------<br>";
+   
+       // echo $matches2[0];
+		print_r($matches3);
+		echo "</pre>";
+		$result_array = preg_match_all("/<w:t xml:space=\"preserve\"><\/w:t>/U",$text,$matches4);
+		
+   echo "<pre>";
+   echo "//---------------------------4444444444444444444444444--------------++++++++++++<br>";
+       // echo $matches2[0]; $a= strip_tags ($matches4[0][1]);
+	    
+		$content = array();
+			for($i=0;$i<=$countMathes;$i++){
+		//print_r($matches4);
+		$textForContent = $matches4[0][$i];
+		//echo $test;
+		$result_array = preg_match("/<w:t>.*<\/w:t>/",$textForContent,$matcheslast);
+		//print_r($matcheslast);
+		
+		$textForContentLast = $matcheslast[0];
+		//echo $test;
+		$result_array = preg_match("/.br.*/",$textForContentLast,$matcheslast1);
+		$content[$i] = $matcheslast1[0];
+			}
+			echo $content[2];
 		echo "</pre>";
